402e7f2e486a63442f4259d61287128644e0c310
harlo <harlo.holmes@gmail.com>
2014-11-11 11:43:39 -0500

    pushing changes

--- image.py
+++ image.py
@@ -14,12 +14,22 @@ def clean_up():
 	return False
 
 def build_image(config):
-	docker_vars = ['SUPER_PACKAGE', 'ANNEX_USER']
-	sec_vars = ['']
+	docker_vars = ['SUPER_PACKAGE', 'ANNEX_USER', 'ANNEX_USER_PWD']
+	sec_vars = ['ssh_root', 'annex_dir', 'uv_server_host', 'uv_uuid']
+	nginx_vars = ['client_max_body_size', 'mode']
 
 	for e in docker_vars + sec_vars:
 		if e not in config.keys():
 			config[e] = prompt("%s: " % e)
+
+	if config['mode'] == 1:
+		# if mode == annex + annex channel
+		config['main_proxy_port'] = 8889
+		config['proxy_extras'] = ''
+	elif config['mode'] == 2:
+		# if mode == frontend + annex + annex channel
+		config['main_proxy_port'] = 8888
+		config['proxy_extras'] = 'proxy_set_header Cookie "$http_cookie;$uv_public";'
 
 	dockerfile = []
 	doc_rx = re.compile("\$\{(%s)\}" % '|'.join(docker_vars))
@@ -30,16 +40,23 @@ def build_image(config):
 			
 			dockerfile.append(line)
 
-	# first, do we have a stock build for the annex user?
 	with open("Dockerfile", 'wb') as t:
 		t.write('\n'.join(dockerfile))
 
-	unveillance_secrets = {}
-	for s in sec_vars:
-		unveillance_secrets[s] = config[s]
+	nginxfile = []
+	ngx_rx = re.compile("\${(%s)\}" % '|'.join(nginx_vars))
+	with open("nginx.conf.example", 'rb') as nginxstub:
+		for line in nginxstub.readlines():
+			for e in re.findall(ngx_rx, line):
+				line = line.replace("${%s}" % e, config[e])
 
+		nginxfile.append(line)
+
+	with open("nginx.conf", 'wb') as t:
+		t.write(''.join(nginxfile))
+	
 	with open("unveillance.secrets.json", 'wb') as t:
-		t.write(dumps(unveillance_secrets))
+		t.write(dumps(config))
 
 	with settings(warn_only=True):
 		image_id = None
@@ -57,23 +74,30 @@ def build_image(config):
 			print "SORRY NO IMAGE"
 			return clean_up()
 
+		'''
 		container_id = None
-		run_cmd = "sudo docker run -d -t && cd %(h)s && ./setup.sh %(h)s/unveillance.secrets.json" % (
-			{'h' : os.path.join(config['ANNEX_USER'], config['SUPER_PACKAGE'])})
+		run_cmd = "sudo docker run -i -t unveillance/stock:%(u)s /bin/bash cd %(h)s && ./setup.sh %(h)s/unveillance.secrets.json" % (
+			{'h' : os.path.join(config['ANNEX_USER'], config['SUPER_PACKAGE']), 'u' : config['ANNEX_USER']}
+		)
 
 		for status in local(run_cmd, capture=True).splitlines():
 			print "XX: %s" % status
 
 		#for status in local("sudo docker commit %s unveillance/annex:blank" % image_id, capture=True).splitlines():
 			#print "XX: %s" % status
+		'''
 
 	return not clean_up()
 
 if __name__ == "__main__":
+	config = {}
+
 	if len(argv) == 2:
-		config = argv[1]
-	else:
-		config = {}
+		try:
+			with open(argv[1], 'rb') as c:
+				config = loads(c.read())
+		except Exception as e:
+			print e
 
 	if build_image(config):
 		exit(0)
